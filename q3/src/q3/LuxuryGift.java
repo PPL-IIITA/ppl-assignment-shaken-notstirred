4c8a1f5e17507a5995ccf871dd80980536d46122
shaken-notstirred <iit2015088@iiita.ac.in>
2017-04-10 02:55:15 +0530

    Add files via upload

---
+++ q3/src/q3/LuxuryGift.java
@@ -0,0 +1,15 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package src.q3;
+
+/**
+ *
+ * @author Pranjal
+ */
+public class LuxuryGift extends GiftClass{
+    int luxuryRating;
+    int difficulty;
+}
---
+++ q3/src/q3/desperate.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package src.q3;
+
+/**
+ *
+ * @author Pranjal
+ */
+public class desperate extends girl{
+    int totcost;
+    int totvalue;
+    int girlhap;
+    
+    void calculateHappiness()
+    {
+        girlhap=totcost+totvalue;
+    }
+}
