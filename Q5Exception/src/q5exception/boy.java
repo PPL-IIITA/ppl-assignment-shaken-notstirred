1e19a9568698a3d14f59533787ee404735dd1c11
shaken-notstirred <iit2015088@iiita.ac.in>
2017-04-19 01:07:13 +0530

    Add files via upload

---
+++ Q5Exception/src/q5exception/boy.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package q5exception;
+
+/**
+ *
+ * @author Pranjal
+ */
+public class boy {
+     String name;
+    int type;
+    int attractiveness;
+    int intelligence;
+    int budget;
+    int taken = 0;
+    int takenfc = 0;
+    int incompatID = -1;    
+}
